db7ee2a02be1dda01820c48d2437936a993e0cfa
zack Davison <zackdavison1@outlook.com>
2024-07-18 21:47:25 +0100

    improving type checking and made json response more detailed

--- Routes/apiRoutes.js
+++ Routes/apiRoutes.js
@@ -1,19 +1,29 @@
+//requiring express
 const express = require('express');
-
+//creating addition router
 const additionRouter = express.Router();
+//requiring type checking utility function
+const {verifyType} = require('../utils');
 
 additionRouter.get('/:value1/:value2', (req, res) => {
     //assigning parameters to variables
     const value1 = Number(req.params.value1);
     const value2 = Number(req.params.value2);
-
-    //constructing response object
-    const responseObject = {
-        result: value1 + value2
-    };
-    //sending json response
-    res.send(JSON.stringify(responseObject));
-
+    
+    if(verifyType('number', value1, value2)){
+        //constructing response object
+        const responseObject = {
+            value1: value1,
+            value2: value2,
+            operator: '+',
+            result: value1 + value2
+        };
+        //sending json response
+        res.status(200).send(JSON.stringify(responseObject));
+    }else{
+        //sending bad request response
+        res.status(400).send();
+    }
 })
 
 module.exports = {
