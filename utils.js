7c005cdd4d33f63ecf75ce8c69accfd38ca0fae6
zack Davison <zackdavison1@outlook.com>
2024-07-18 21:44:03 +0100

    Changed number of arguments in verifyType function

--- utils.js
+++ utils.js
@@ -1,5 +1,5 @@
 //function that validates both values are correct type
-const verifyType = (type, value1, value2, value3 = 0, value4 = 0) => {
+const verifyType = (type, value1, value2) => {
     //checking if both values are of correct type and if so returning true
     if(typeof value1 !== type || typeof value2 !== type ){
         return false;
