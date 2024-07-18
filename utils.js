e424909fa9c9536921f7bafc707014243e863b80
zack Davison <zackdavison1@outlook.com>
2024-07-18 21:42:06 +0100

    Changed name of verifyNumber function to verifyType and changed number of arguments allowed

--- utils.js
+++ utils.js
@@ -1,7 +1,7 @@
 //function that validates both values are correct type
-const verifyNumber = (type, value1, value2, value3 = 0, value4 = 0) => {
+const verifyType = (type, value1, value2, value3 = 0, value4 = 0) => {
     //checking if both values are of correct type and if so returning true
-    if(typeof value1 !== type || typeof value2 !== type || typeof value3 !== type || typeof value4 !== type){
+    if(typeof value1 !== type || typeof value2 !== type ){
         return false;
     }else{
         return true;
